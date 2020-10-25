a31c5f4f046ea18607af6aaeb70c4e85f9c97d57
Lefteris Chatziefraimidis <echatzief97@gmail.com>
2020-10-25 16:34:00 +0200

    The main window with some network checks

--- dashboard/app/src/components/App.js
+++ dashboard/app/src/components/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import '../static/css/App.css';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
 import Loading from './Loading';
+import WrongAP from './WrongAP';
+import MainDisplay from './MainDisplay';
+import '../static/css/App.css';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [isConnectedToVrada, setIsConnectedToVrada] = useState(false);
+
+  const checkTheWifi = () => {
+    const url = queryString.parse(window.location.search);
+    console.log(url.ssid);
+    if (url.ssid.toUpperCase().includes('VRADA')) {
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (checkTheWifi()) {
+      setLoading(false);
+      setIsConnectedToVrada(true);
+    } else {
+      setLoading(false);
+      setIsConnectedToVrada(false);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Loading />
+      {
+        isLoading
+          ? <Loading isLoading={isLoading} />
+          : (
+            <div>
+              {
+              isConnectedToVrada
+                ? <MainDisplay />
+                : <WrongAP />
+            }
+            </div>
+          )
+      }
     </div>
   );
 }
