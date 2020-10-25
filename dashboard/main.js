5cd163c2e94bb91700ef38571f97b54a5a6765c8
Lefteris Chatziefraimidis <echatzief97@gmail.com>
2020-10-25 16:31:05 +0200

    Added the start stop functionality that the user can write to a csv the data

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,5 +1,36 @@
-const { app, BrowserWindow } = require('electron')
-const { getSSID } = require("./utils/index")
+const { app, BrowserWindow, ipcMain,dialog } = require('electron')
+const { fstat } = require('fs')
+const { getSSID } = require("./utils/ssid")
+const UDPListener = require('./utils/UDPListener')
+const { isWifiChanged } = require('./utils/wifi')
+const fs = require('fs')
+
+var WRITE_TO_CSV = false
+var CSV_FILENAME = ""
+
+// Create the udp listener
+const eventListener = new UDPListener().Listen()
+eventListener.on('udp-packets',(packet)=>{
+  // Split the packet info
+  if(WRITE_TO_CSV && CSV_FILENAME){
+    const packetData = packet.toString().split(";")
+    console.log({
+      "Speed":packetData[2].split(":")[1],
+      "rpm":packetData[3].split(":")[1],
+      "distance":packetData[4].split(":")[1],
+      "calories":packetData[5].split(":")[1],
+      "hf":packetData[6].split(":")[1],
+      "power":packetData[7].split(":")[1],
+      "level":packetData[8].split(":")[1],
+    })
+    let d =`${new Date().toLocaleTimeString()},${packetData[1]},${packetData[2].split(":")[1]},${packetData[3].split(":")[1]},`
+          +`${packetData[4].split(":")[1]},${packetData[5].split(":")[1]},${packetData[6].split(":")[1]},${packetData[7].split(":")[1]},`
+          +`${packetData[8].split(":")[1]}\n`
+    fs.appendFileSync(CSV_FILENAME,d)
+  }
+})
+
+
 
 async function createWindow () {
   const win = new BrowserWindow({
@@ -16,10 +47,42 @@ async function createWindow () {
     const winURL = `http://localhost:8080?ssid=${await getSSID()}`
     win.loadURL(winURL)
   } else {
-    win.loadFile(__dirname+'/app/dist/index.html');
+    win.loadFile(__dirname+`/app/dist/index.html?ssid=${await getSSID()}`);
   }
   
   win.webContents.openDevTools()
+
+  // Start the write to csv
+  ipcMain.on('START_WRITE',async ()=>{
+    try{
+      console.log("Start to write.....")
+      WRITE_TO_CSV = true
+
+      // Ask for the dialog
+      let res = await dialog.showSaveDialog({
+        defaultPath: new Date().toLocaleString() + ".csv"
+      })
+      if(res.canceled){
+        CSV_FILENAME = ""
+      } else {
+        CSV_FILENAME = res.filePath
+      }
+      fs.writeFileSync(CSV_FILENAME,"Ημερομηνία,Συσκευή,Ταχύτητα,RPM,Απόσταση,Θερμίδες,Καρδιακοί Παλμοί,Ισχύς,Επίπεδο\n")
+    } catch(e){
+      console.log(e)
+      dialog.showErrorBox("Σφάλμα", "Σφάλμα κατά την αποθήκευση");
+    }
+  })
+
+  // Stop the write to csv
+  ipcMain.on('STOP_WRITE',()=>{
+    console.log("Stopped the writing....")
+    WRITE_TO_CSV = false
+    CSV_FILENAME = ""
+  })
+  
+  // Interval that checks the wifi changes 
+  isWifiChanged(win)
 }
 
 app.whenReady().then(createWindow)
